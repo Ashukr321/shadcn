886b6b50d716c62d3571ab6ea7d9d6565a49f17e
Ashukr321 <coderashukr321@gmail.com>
2024-03-11 01:10:25 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main  className="p-10" >
+      <h1 className="text-blue-600 text-center text-3xl">welcome to learn about the shadcn</h1>
+    </main>
+  );
+}
