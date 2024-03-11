87cf8b481d5ce0c1d3391a7554f711fc61e12d7d
Ashukr321 <coderashukr321@gmail.com>
2024-03-12 00:31:09 +0530

    how to implement on the dark mode in the shadcn

--- src/components/Typo.jsx
+++ src/components/Typo.jsx
@@ -7,7 +7,7 @@ const Typo = () => {
       To learn more about "shadcn" and its similarities or differences with Tailwind CSS, Radix, or other UI component frameworks, I recommend checking the latest online sources such as official documentation, community forums, or the project's website. Look for comparisons, features, and user reviews to get a better understanding of how "shadcn" fits into the landscape of UI component frameworks.
     </p>
 
-    <h1  className='mt-2 text-xl font-bold'>list in the shadcn/ui components</h1>
+    <h1  className='mt-2 text-xl font-bold dark:text-orange-500 dark:tracking-wide'> ⭐ list in the shadcn/ui components 🧑‍💻</h1>
     <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
       <li>accordian</li>
       <li>aleart</li>
