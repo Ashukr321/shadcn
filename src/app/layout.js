081fbe6d77eee1727edd535d39c4477ee556e550
Ashukr321 <coderashukr321@gmail.com>
2024-03-11 23:14:56 +0530

    add theam mode change completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
+import DarkTh from "@/components/DarkTh";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      > 
+     
+      <div style={{position:'fixed', bottom:0, padding:10}}>
+      <DarkTh   />
+      </div>
+      {children}
+
+      </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,15 @@ import BtnCompoents from "@/components/BtnCompoents";
 export default function Home() {
   return (
    <div>
-      <main className="container py-10 h-screen bg-slate-800">
+      <main className="px-4 pt-14 sm:px-10 h-screen zIndex: 1000">
        <section className="p-4 rounded-sm bg-red-300">
          <BtnCompoents/>
        </section>
        <section className="mt-4 flex justify-center bg-orange-300 p-4 rounded-sm">
          <AlertDialog/>
        </section>
+
+       
       </main>
    </div>
   );
